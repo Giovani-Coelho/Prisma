e704b895d39b45489084270361dd29e7e03e9a63
Giovani Coelho <giovanicoelho2004@hotmail.com>
2023-01-04 23:40:51 -0300

    feat: search findMany

---
+++ src/manyToMany/search/findByCourse.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+async function main() {
+  const result = await prisma.course.findMany({
+    where: {
+      id: "83c9b668-f3f2-4bda-aeb5-1cc3ea5ce8f0"
+    },
+    include: {
+      CoursesModules: {
+        include: {
+          module: true
+        }
+      }
+    }
+  })
+  console.log(JSON.stringify(result))
+}
+
+main()
